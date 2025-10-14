b05973377f8aef62bc92296ac3e0223be4ffbd7d
komal-pb <komal03.pb@gmail.com>
2025-10-15 01:30:47 +0530

    backend update

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load .env variables
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root@123",
-  database: "todo_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
